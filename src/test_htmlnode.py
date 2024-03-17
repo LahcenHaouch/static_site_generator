58800b40c65c9616432b3fc4953204ed7d718273
LahcenHaouch <lahcen.haouch@protonmail.com>
2024-03-17 21:17:13 +0100

    Create htmlnode and test props_to_html

---
+++ src/test_htmlnode.py
@@ -0,0 +1,11 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+
+    def test_props_to_html(self):
+        node = HTMLNode("p", "Hello world", props={"class": "px-2", "id": "test"})
+
+        self.assertEqual(node.props_to_html(), " class=px-2 id=test")
