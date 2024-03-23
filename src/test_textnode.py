722b4990404fceae2ee84d08d063cba8b6675240
LahcenHaouch <lahcen.haouch@protonmail.com>
2024-03-24 00:18:20 +0100

    Add split_nodes_delimiter and test case

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -13,6 +13,19 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold", "url")
         node2 = TextNode("This is a text node", "bold")
         self.assertNotEqual(node, node2)
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        new_nodes = split_nodes_delimiter([node], "`", "code")
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", "text"),
+                TextNode("code block", "code"),
+                TextNode(" word", "text"),
+            ],
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -39,3 +39,55 @@ def text_node_to_html_node(text_node):
             )
         case _:
             raise Exception("WTF man!")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+
+    for old_node in old_nodes:
+        if not isinstance(old_node, TextNode):
+            result.append([old_node])
+            continue
+
+        is_valid, words = is_text_node_valid(old_node, delimiter, text_type)
+
+        if not is_valid:
+            raise Exception(f"Invalid text: {old_node.text}")
+        else:
+            result.extend(words)
+
+    return result
+
+
+def is_text_node_valid(node, delimiter, text_type):
+    stack = []
+    word_stack = []
+    result = []
+
+    text = node.text
+    for i in range(len(text)):
+        char = text[i]
+        in_delimiter = len(stack) >= 1
+
+        if char == delimiter:
+            if in_delimiter:
+                prev_index = stack.pop()
+                result.append(TextNode(text[prev_index:i], text_type))
+
+            else:
+                stack.append(i + 1)
+        else:
+            if in_delimiter:
+                continue
+            else:
+                if (
+                    i + 1 >= len(text)
+                    or text[i + 1] == delimiter
+                    and len(word_stack) >= 1
+                ):
+                    prev = word_stack.pop()
+                    result.append(TextNode(text[prev : i + 1], "text"))
+                elif len(word_stack) == 0:
+                    word_stack.append(i)
+
+    return len(stack) == 0 and len(word_stack) == 0, result
